905222e6cca18744af99f0bde5988fdabea30378
wataru5354 <setsunan76253@gmail.com>
2022-04-13 12:46:36 +0900

    宇宙船の完成

--- settings.py
+++ settings.py
@@ -7,3 +7,12 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        # 宇宙船の設定
+        self.ship_speed = 1.5
+
+        #弾の設定
+        self.bullet_speed = 1.0
+        self.bullet_width = 3.0
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullet_allowed = 3
